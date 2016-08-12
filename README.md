57862bdfa4d2ffd3c81902da5d2b65051d10eb56
Binu Panicker <bpanicker13@gmail.com>
2016-08-11 23:35:21 -0500

    new commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<img style="width:100%;" src="/github-banner.png">
+
+# [Rise of the Nodebots](http://bots.nodejshouston.com/)
+
+## What is Nodebots Day?
+
+Come be a part of an International NodeBots Day hosted here in Houston, TX. July 30th is the NodeBots day for 2016, which is an annual event where people gather together to hack on, or learn to hack on, hardware using Javascript. For additional information on the event, please visit: nodebots.io.
+
+The event is organized by several houston community leaders including Node.js Houston, Houston Robotics Club, and Houston IoT Meetup.
+
+## Venue
+
+University of Houston
+
+University of Houston Classroom and Business Building 
+4742 Calhoun Rd. Houston, TX 77204
+
+## Get Involved
+
+Also, to stay up-to-date on all NodeJS Houston related news and the community you should definitely [chat with us on Slack](http://chat.nodejshouston.com), [follow us on Twitter](https://twitter.com/nodejshouston), and check out website at [NodeJS Houston](http://www.nodejshouston.com).
+
+### License
+
+The MIT license.
---
+++ public/js/script.js
@@ -0,0 +1,118 @@
+// Realtime communication between the client and server uses
+// http://socket.io/
+var socket = io();
+
+// For keypresses we use
+// https://github.com/dmauro/Keypress
+var listener = new window.keypress.Listener();
+
+// For the joystick we use
+// https://github.com/jeromeetienne/virtualjoystick.js
+var stick = new VirtualJoystick({
+  container: document.getElementById("stickContainer"),
+  strokeStyle: 'yellow',
+  limitStickTravel: true
+});
+
+stick.addEventListener('touchStartValidation', function(event) {
+  var touch	= event.changedTouches[0];
+  return true;
+});
+
+// Store the last sent drive values
+var lastForward, lastAz;
+
+// Check the position of the joystick every 100ms
+setInterval( function() {
+
+  var left, right;
+
+  var rawForward = Number(stick.deltaY() / 100);
+  var rawAz = Number(stick.deltaX() / 100);
+
+  // If the position has changed
+  if (rawForward !== lastForward || rawAz !== lastAz) {
+
+    var easedAz = ease(Math.abs(rawAz));
+
+    if (rawAz < 0) easedAz *= -1;
+
+    // The maxDiff is the difference between 1 and our forward speed
+    var maxDiff = rawForward < 0 ? (-1 - rawForward) : (1 - rawForward);
+
+    var diff = easedAz >= 0 ? lesser(maxDiff, easedAz): greater(maxDiff, easedAz);
+
+    if ((rawForward < 0 && easedAz > 0) || (rawForward > 0 && easedAz < 0)) {
+      right = rawForward + diff;
+      left = rawForward - diff;
+    }
+    if ((rawForward < 0 && easedAz < 0) || (rawForward > 0 && easedAz > 0)) {
+      right = rawForward - diff;
+      left = rawForward + diff;
+    }
+
+    // If the user has released the joystick
+    if (rawForward === 0 && easedAz === 0) {
+      socket.emit("stop");
+    } else {
+      socket.emit("drive", {left: left, right: right});
+    }
+
+    lastForward = rawForward;
+    lastAz = rawAz;
+  }
+
+}, 1/10 * 1000);
+
+// These are the commands that are bound to
+// buttons and keypresses
+var commands = [
+  {"com": "forward", "key": "up" },
+  {"com": "left", "key": "left" },
+  {"com": "right", "key": "right" },
+  {"com": "reverse", "key": "down" },
+  {"com": "stop", "key": "space" }
+];
+
+// Loop through each of our commands
+commands.forEach( function(command) {
+
+  // Add listeners on the buttons
+  command.el = document.getElementById(command.com);
+  command.el.addEventListener("touchstart", function() {
+    socket.emit(command.com);
+    return false;
+  });
+  command.el.addEventListener("touchend", function() {
+    socket.emit("stop");
+  });
+
+  // Listen for keypress events
+  listener.register_combo({
+    "keys"              : command.key,
+    "on_keydown"        : function() { socket.emit(command.com); },
+    "on_release"        : function() { socket.emit("stop"); },
+    "prevent_default"   : true,
+    "prevent_repeat"    : true
+  });
+
+});
+
+// Bind event to window so the event works even when the mouse is outside browser
+document.addEventListener("mouseup", function() {
+    socket.emit("stop");
+});
+
+// Helper functions
+function ease(n) {
+  return n;
+  //return n * (2 - n);
+}
+
+function greater(a, b) {
+  return a > b ? a : b;
+}
+
+function lesser(a, b) {
+  return a > b ? b : a;
+}
